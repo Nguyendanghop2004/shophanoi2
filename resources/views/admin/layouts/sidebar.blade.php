2fa0448cebd2ca3395ee308eb28545ab6133f17b
huunamvn <tahuunam017@gmail.com>
2024-10-10 14:33:11 +0700

    Update sidebar.blade.php

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -238,10 +238,10 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Bảng Điều Khiển</li>
-            <li class="dropdown active">
+            <li class="dropdown ">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Thống Kê</span></a>
                 <ul class="dropdown-menu">
-                    <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                    <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
                     <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
             </li>
@@ -280,10 +280,5 @@
             </li>
         </ul>
 
-        <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                <i class="fas fa-rocket"></i> Documentation
-            </a>
-        </div>
     </aside>
 </div>
