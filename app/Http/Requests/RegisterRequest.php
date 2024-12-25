6f22db7f158046c0ae0b7eda15cd452c395c21b0
nguyen dang hop <hopndph41769@fpt.edu.vn>
2024-12-26 01:13:54 +0700

    edit user

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,6 +25,30 @@ class RegisterRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required|string|min:8',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'Tên là trường bắt buộc.',
+            'name.string' => 'Tên phải là một chuỗi ký tự.',
+            'name.max' => 'Tên không được vượt quá 255 ký tự.',
+
+            'email.required' => 'Email là trường bắt buộc.',
+            'email.string' => 'Email phải là một chuỗi ký tự.',
+            'email.email' => 'Email phải là một địa chỉ email hợp lệ.',
+            'email.max' => 'Email không được vượt quá 255 ký tự.',
+            'email.unique' => 'Email đã được sử dụng.',
+
+            'password.required' => 'Mật khẩu là trường bắt buộc.',
+            'password.string' => 'Mật khẩu phải là một chuỗi ký tự.',
+            'password.min' => 'Mật khẩu phải có ít nhất 8 ký tự.',
+            'password.confirmed' => 'Mật khẩu và xác nhận mật khẩu không khớp.',
+
+            'password_confirmation.required' => 'Xác nhận mật khẩu là trường bắt buộc.',
+            'password_confirmation.string' => 'Xác nhận mật khẩu phải là một chuỗi ký tự.',
+            'password_confirmation.min' => 'Xác nhận mật khẩu phải có ít nhất 8 ký tự.',
         ];
     }
 }
